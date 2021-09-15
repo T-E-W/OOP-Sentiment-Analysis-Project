b92f7118bd63e9d59f82a6f2708ffe86c67617c3
TimDubya <twebb3@cub.uca.edu>
2021-09-15 13:58:45 -0500

    1st commit. Added basic structure

---
+++ Project 1/project1/Tweet.java
@@ -0,0 +1,21 @@
+package project1;
+
+public class Tweet {
+	private String polarity;
+	private String id;
+	private String user;
+	private String bodyText;
+	
+	public Tweet(String polarity, String id, String user, String bodyText) {
+		super();
+		this.polarity = polarity;
+		this.id = id;
+		this.user = user;
+		this.bodyText = bodyText;
+	}
+	
+	
+	
+	
+	
+}
---
+++ Project 1/project1/TweetCollection.java
@@ -0,0 +1,38 @@
+package project1;
+
+public class TweetCollection {
+
+	
+	// Add New Tweet To Collection
+	public void add() {
+		
+	}
+	
+	// Retrieve tween based off ID;
+	public String get(String rid) {
+		
+		return null;
+	}
+	
+	// Return all IDs of all the Tweets in the Collection
+	public String getIds() {
+		return null;
+	}
+	
+	// Return all IDs of tweets from specific User Name
+	public String getUserTweets() {
+		return null;
+	}
+	
+	// Delete/Remove tween using ID from collection.
+	public void remove() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "TweetCollection [polarity=" + polarity + ", id=" + id + ", user=" + user + ", bodyText=" + bodyText
+				+ "]";
+	}
+	
+}
